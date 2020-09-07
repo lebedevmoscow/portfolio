62f1b3ca8edbc0ec0ce4b8866028de0a5d5ca6c9
Boris Lebedev <global-mangal@yandex.ru>
2020-09-07 23:46:48 +0500

    Setup base project architecture

---
+++ pages/_app.js
@@ -0,0 +1,9 @@
+import './../styles/index.scss'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+const MyApp = ({Component, pageProps}) => {
+    
+    return <Component {...pageProps}/>
+}
+
+export default MyApp
